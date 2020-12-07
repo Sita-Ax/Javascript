e23295a46253405e92ca2ae35b6b3f49e230e4ae
Sita-Ax <69907961+Sita-Ax@users.noreply.github.com>
2020-12-07 08:08:06 +0100

    Add files via upload

--- Rosita_Axelsson.js
+++ Rosita_Axelsson.js
@@ -1,30 +1,80 @@
-"use strict";
+'use strikt'
+//user can only login with these names.
+let usernames = ['Rosita', 'ec', 'softhouse'];
 
-function Book(Book, Topic, Pages, ISBN) {
-    this.Book = Book;
-    this.Topic = Topic;
-    this.Pages = Pages;
-    this.ISBN = ISBN;
-    this.getBookInfo = function() {
-        console.log('Book: ' + this.Book + '\nTopic: ' + this.Topic + '\nPages: ' + this.Pages + '\nISBN: ' + this.ISBN);
-        console.log('------------\n');
-    };
+//Function that calls all other functions in order
+//--Don't modify--
+function init() {
+    initLoginForm();
+    initLabel();
+    initInput();
+    initButton();
+}
+let form;
+let label;
+let input;
+let btn;
+
+function initLoginForm() {
+    form = document.createElement('form');
+    document.body.appendChild(form);
+    //Add code here to:
+    //create and add form element to the Dom.
 }
 
-let books = [];
-books.push(new Book('Pippi', 'Childrensbook', '159', '978-91-7004-152-7'));
-books.push(new Book('Emil', 'Childrensbook', '205', '978-91-7007-150-0'));
-books.push(new Book('Ronja Rövadotter', 'Childrensbook', '300', '978-91-7002-158-5'));
-
-let amount = prompt("How many books are you select?", " ");
-for (let i = 0; i < amount; i++) {
-    let book = prompt('Book: ', '');
-    let Topic = prompt('Topic: ', '');
-    let Pages = parseInt(prompt('Pages: ', ''));
-    let ISBN = parseInt(prompt('ISBN: ', ''));
-    books.push(new Book(book, Topic, Pages, ISBN));
+function initLabel() {
+    label = document.createElement('label');
+    label.textContent = 'Username';
+    form.appendChild(label);
+    //Add code here to:
+    //create label element and set the lable to "username" (TIP: use .textContent).
+    //Add it to the form element. (TIP: use .appendChild).
 }
 
-books.forEach(function(e) {
-    e.getBookInfo();
+function initInput() {
+    input = document.createElement('input');
+    input.required = true;
+    form.appendChild(input);
+    //Add code here to:
+    //create input element and set it to be requried (TIP: use .required = true).
+    //Add it to the form element. (TIP: use .appendChild).
+}
+
+function initButton() {
+    btn = document.createElement('BUTTON');
+    btn.textContent = 'Login';
+    form.appendChild(btn);
+    btn.addEventListener('click', () => {
+        if (validateLoginInfo() == true) {
+            alert('Welcome!');
+        } else {
+            input.style.border = '3px, solid red';
+        }
+    });
+
+    //create button element and set the text to "login" (TIP: use .textContent)
+    ////Add it to the form element. (TIP: use .appendChild).
+
+    //Add click eventListener for button and call validateLoginInfo function inside it.
+    //if validateLoginInfo is true alert user with "Welcome" text.
+    //if validateLoginInfo is false add border style to the input element: "3px solid red". (TIP: use .style.border).
+}
+
+function validateLoginInfo() {
+
+    for (let i = 0; i < usernames.length; i++) {
+        if (usernames[i] === input.value) {
+            return true;
+        }
+    }
+    return false;
+
+    //Get the input using form input element (TIP: use .value)
+    //if the username is in the usernames array return true if not return false.
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    init();
 });
+
+//Write code here to call the init function only after the dom has been loaded (TIP: check "DOMContentLoaded" event)
